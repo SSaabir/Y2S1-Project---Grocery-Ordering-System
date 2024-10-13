6d01abb712df303592c31dfc60cd100d39016225
SSaabir <159421259+SSaabir@users.noreply.github.com>
2024-10-13 21:09:35 +0530

    chhhh

--- FreshCoN/src/main/java/freshco/Control/Checkout.java
+++ FreshCoN/src/main/java/freshco/Control/Checkout.java
@@ -2,6 +2,8 @@ package freshco.Control;
 
 import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,8 +25,9 @@ public class Checkout extends HttpServlet {
         // Check if cart items exist
         if (cartItems == null || cartItems.isEmpty()) {
             // Redirect to an error page or empty cart page if no items are present
-            response.sendRedirect("emptyCart.jsp");
-            return;
+        	request.setAttribute("errorMessage", "Failed to add Admin. Email already exists. Please try again.");
+        	RequestDispatcher dispatcher1 = request.getRequestDispatcher("Shop");
+        	dispatcher1.forward(request, response);
         }
 
         // Set cart items as request attribute to access in JSP
